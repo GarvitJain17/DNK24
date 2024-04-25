f03abaf3566c62a835244e60ed7a473bfb120420
GarvitJain <jaingarvit80@gmail.com>
2024-04-26 02:01:38 +0530

    iNITIAL COMMIT

---
+++ src/routes/user.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/user.js";
+import { get } from "http";
+import { adminOnly } from "../middlewares/auth.js";
+const app = express.Router();
+
+// route - /api/v1/user/new
+app.post("/new",newUser)
+// route - /api/v1/user/all
+app.get("/all",adminOnly,getAllUsers)
+// route - /api/v1/user/getUser
+//chaining of routes
+app.route("/:id").get(adminOnly,getUser).delete(adminOnly,deleteUser)
+export default app ;
+
